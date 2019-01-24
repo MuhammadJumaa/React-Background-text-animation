1f0b4064ccb3bfdbcc6b22fd780a1035dd90ea8d
mohammad.jumaa <codepassion89@gmail.com>
2019-01-24 18:00:40 +0300

    Updates

---
+++ precache-manifest.c61664656a3c47ba3e18b57486dc0ccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c529e8851708cc486d3d16be3e71202f",
+    "url": "/React-Background-text-animation/static/media/img.c529e885.png"
+  },
+  {
+    "revision": "af7a7b451dd26f0a0b10",
+    "url": "/React-Background-text-animation/static/js/runtime~main.af7a7b45.js"
+  },
+  {
+    "revision": "73eca99a697c13724b50",
+    "url": "/React-Background-text-animation/static/js/main.73eca99a.chunk.js"
+  },
+  {
+    "revision": "f26e08eda90f47944d12",
+    "url": "/React-Background-text-animation/static/js/1.f26e08ed.chunk.js"
+  },
+  {
+    "revision": "73eca99a697c13724b50",
+    "url": "/React-Background-text-animation/static/css/main.1714744d.chunk.css"
+  },
+  {
+    "revision": "9f1d1871d202ffe8388904927d19b6b4",
+    "url": "/React-Background-text-animation/index.html"
+  }
+];
